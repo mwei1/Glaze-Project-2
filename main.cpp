5b6c6c8095f4b06abafa53c700f3d5723a42a2d9
dipplydopply <michaelwei251@gmail.com>
2026-03-20 13:59:26 -0400

    added muting/unmuting functionality to pause music

--- main.cpp
+++ main.cpp
@@ -2,19 +2,34 @@
 // Created by lawre on 3/2/2026.
 //
 #include <iostream>
-#include "CSVReader.h"
-#include <vector>
-#include "Trie.h"
-
 #include <optional>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+#include "CSVReader.h"
+#include <vector>
+#include "Trie.h"
+
 using namespace std;
+
+void musicSwitch(sf::Music& music, sf::Sprite& spr, sf::Texture& mute, sf::Texture& unmute)
+{
+    if (music.getStatus() == sf::Sound::Status::Playing)
+    {
+        music.pause();
+        spr.setTexture(mute);
+    }
+    else
+    {
+        music.play();
+        spr.setTexture(unmute);
+    }
+}
 
 int main() {
 
     sf::RenderWindow window(sf::VideoMode({1200, 800}), "Dessert Search");
+    window.clear(sf::Color::White);
 
     //load window icon
     sf::Image icon;
@@ -26,6 +41,19 @@ int main() {
     sf::Music music("resources/audio/Music.mp3");
     music.setLooping(true);
     music.play();
+
+    //buttons
+    //sf::Sprite toggleDataStructure;
+
+    sf::Texture unmuted;
+    unmuted.loadFromFile("resources/images/unmute.png");
+
+    sf::Texture muted;
+    muted.loadFromFile("resources/images/mute.png");
+
+    sf::Sprite toggleMusic(unmuted);
+    toggleMusic.setScale(sf::Vector2f(0.2, 0.2));
+    toggleMusic.setPosition({0.f, 750.f});
 
 
     vector<Recipe> recipes = CSVReader::loadRecipesFromFile("resources/recipes.csv");
@@ -63,14 +91,27 @@ int main() {
 
     while (window.isOpen())
     {
+        sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+        if (toggleMusic.getGlobalBounds().contains(mousePos))
+            toggleMusic.setScale(sf::Vector2f(0.18, 0.18));
+        else
+            toggleMusic.setScale(sf::Vector2f(0.2, 0.2));
+
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+            if (event->is<sf::Event::MouseButtonReleased>())
+            {
+                if (toggleMusic.getGlobalBounds().contains(mousePos))
+                    musicSwitch(music, toggleMusic, muted, unmuted);
+            }
         }
 
-        window.clear();
-
+        window.clear(sf::Color::White);
+        window.draw(toggleMusic);
         window.display();
     }
 
